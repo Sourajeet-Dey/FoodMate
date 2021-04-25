ed64496362ebd098670b5559681bd66165352dfa
imniladri <jnvniladri101@gmail.com>
2021-04-25 16:50:54 +0530

    Redesign Header & Banner

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FoodMate
+
 It is a basic project for food delivery things
+[FoodMate](https://sourajeet-dey.github.io/FoodMate/)
---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Loader Event
+$(window).on("load", function () {
+    $("#preloader").css({
+        transform: "translateY(-100%)",
+        "transition-delay": "0.6s",
+    });
+    $(".loader").css({
+        opacity: "0",
+        "transition-delay": "0.3s",
+    });
+});
+
+// Scroll Event
+$(window).scroll(function () {
+    $("header").toggleClass("showHeader", window.scrollY > 50);
+});
+
+// Navbar Toggle
+$(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("showNavbar");
+});
+
+// Cart Button Mouse Hover In
+$(".cart-link").mouseenter(function () {
+    $(".cart-link img").attr("src", "./img/cart_light.png");
+});
+
+// Cart Button Mouse Hover Out
+$(".cart-link").mouseleave(function () {
+    $(".cart-link img").attr("src", "./img/cart_dark.png");
+});
+
+// Go Back Function
+$(".go-back").click(function () {
+    window.history.back();
+});
