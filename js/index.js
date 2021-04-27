fd15cce850cde46f1e7e69477a2b876a8110cb2b
imniladri <jnvniladri101@gmail.com>
2021-04-27 20:46:10 +0530

    Added Cart Counter

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Loader Event
+
 $(window).on("load", function () {
     $("#preloader").css({
         transform: "translateY(-100%)",
@@ -10,32 +11,48 @@ $(window).on("load", function () {
     });
 });
 
+// ****************************************************************************
+
 // Scroll Event
+
 $(window).scroll(function () {
     $("header").toggleClass("showHeader", window.scrollY > 50);
 });
 
+// ****************************************************************************
+
 // Navbar Toggle
+
 $(".navbar-toggler").click(function () {
     $(".navbar-toggler").toggleClass("showNavbar");
 });
 
+// ****************************************************************************
+
 // Cart Button Mouse Hover In
+
 $(".cart-link").mouseenter(function () {
     $(".cart-link img").attr("src", "./img/cart_light.png");
 });
 
 // Cart Button Mouse Hover Out
+
 $(".cart-link").mouseleave(function () {
     $(".cart-link img").attr("src", "./img/cart_dark.png");
 });
 
+// ****************************************************************************
+
 // Go Back Function
+
 $(".go-back").click(function () {
     window.history.back();
 });
 
+// ****************************************************************************
+
 // Scroll To Top
+
 $(window).scroll(function () {
     $(".ScrolltoTop").toggleClass("show", window.scrollY > 500);
 });
@@ -44,7 +61,100 @@ $(".ScrolltoTop").click(function () {
     $(window).scrollTop(0);
 });
 
-// Add to Cart Button (Food Cart)
+// ****************************************************************************
+
+// Add to Cart Counter (Food Cart)
+
+var counter = 0;
+var cartCounter = localStorage.getItem("cartCounter");
+
+// Local Storage
+
+// if (cartCounter === "enabled") {
+//     counter++;
+//     $(".badge-count").text(counter);
+//     localStorage.setItem("cartCounter", "enabled");
+// }
+
 // $(".food-cart").click(function () {
-//     $(".food-cart").toggleClass("addCart");
+//     var cartCounter = localStorage.getItem("cartCounter");
+
+//     if (cartCounter !== "enabled") {
+//         counter++;
+//         $(".badge-count").text(counter);
+//         localStorage.setItem("cartCounter", "enabled");
+//     } else {
+//         counter--;
+//         $(".badge-count").text(counter);
+//         localStorage.setItem("cartCounter", null);
+//     }
 // });
+
+// If / Else
+
+// $(".food-cart").click(function () {
+//     if ($(".food-cart img:nth-child(1)").css("visibility", "visible")) {
+//         counter++;
+//         $(".badge-count").text(counter);
+//     }
+//     else if ($(".food-cart img:nth-child(1)").css("visibility", "hidden")) {
+//         counter--;
+//         $(".badge-count").text(counter);
+//     }
+// });
+
+$(".food-cart img:nth-child(1)").click(function () {
+    counter++;
+    $(".badge-count").text(counter);
+});
+
+$(".food-cart img:nth-child(2)").click(function () {
+    counter--;
+    $(".badge-count").text(counter);
+});
+
+// ****************************************************************************
+
+// Add to Cart Btn: Each Dishes Individual Id
+
+$("#dish_1 img").click(function () {
+    $("#dish_1").toggleClass("addCart");
+});
+
+$("#dish_2 img").click(function () {
+    $("#dish_2").toggleClass("addCart");
+});
+
+$("#dish_3 img").click(function () {
+    $("#dish_3").toggleClass("addCart");
+});
+
+$("#dish_4 img").click(function () {
+    $("#dish_4").toggleClass("addCart");
+});
+
+$("#dish_5 img").click(function () {
+    $("#dish_5").toggleClass("addCart");
+});
+
+$("#dish_6 img").click(function () {
+    $("#dish_6").toggleClass("addCart");
+});
+
+$("#dish_7 img").click(function () {
+    $("#dish_7").toggleClass("addCart");
+});
+
+$("#dish_8 img").click(function () {
+    $("#dish_8").toggleClass("addCart");
+});
+
+$("#dish_9 img").click(function () {
+    $("#dish_9").toggleClass("addCart");
+});
+
+$("#dish_10 img").click(function () {
+    $("#dish_10").toggleClass("addCart");
+});
+
+// ****************************************************************************
